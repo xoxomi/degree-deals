039f50aa7d149a2a449fdb08c76a0475c6b5a2d6
xoxomi <131605271+xoxomi@users.noreply.github.com>
2024-01-28 20:36:45 +0800

    gogogogo

--- src/Screens/Components/Notifications/Messeges.js
+++ src/Screens/Components/Notifications/Messeges.js
@@ -1,10 +1,10 @@
 import { Center, Text, View} from "native-base";
 import React from 'react'
 
-export default function Messeges({ bg, color, children, size}) {
+export default function Messeges({ bg, color, children, size, bold}) {
   return (
     <Center bg={bg} p={4} rounded={5}>
-      <Text color={color} fontSize={size}>
+      <Text color={color} fontSize={size} bold={bold}>
         {children}
       </Text>
     </Center>
--- src/Screens/Components/Review.js
+++ src/Screens/Components/Review.js
@@ -1,10 +1,11 @@
 import { View, Text,} from 'react-native'
-import React from 'react'
-import {Box, Heading } from "native-base"
+import React, { useState } from 'react'
+import {Box, Heading, VStack, FormControl, Select, CheckIcon} from "native-base"
 import Colors from '../../data/Colors'
 import Messeges from './Notifications/Messeges';
 
 export default function Review ()  {
+    const [ratings, setRatings] = useState ('')
   return (
   <Box my={9}>
         <Heading bold fontSize={19} mb={2}>
@@ -13,26 +14,57 @@ export default function Review ()  {
         <Messeges color={Colors.white}
                 bg={Colors.blue}
                 size={15}
+                bold 
                 children={
                     "NO REVIEWS"
                 }
                 >
-
-                </Messeges>
+         </Messeges>
         <Box p={3} bg={Colors.steelblue} mt={5} rounded={5}>
                 <Heading fontSize={15} color={Colors.black}>
                         User Prtty
                 </Heading>
                 <Text my={2} fontSize={12}>Jan 08, 2024</Text>
                 <Messeges color={Colors.black}
-                bg={Colors.white}
+                bg={Colors.grey}
                 size={10}
+                bold
                 children={
                     "Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit..."
                 }
                 >
-
                 </Messeges>
+        </Box>
+        <Box mt={6}>
+                <Heading>
+                    REVIEW THIS PRODUCT
+                </Heading>
+                <VStack space={6}>
+                    <FormControl>
+                        <FormControl.Label 
+                        _text={{
+                            fontSize: "12px",
+                            fontWeight: "bold",
+                        }}>
+                        </FormControl.Label>
+                        Rating
+                    </FormControl>
+                    <Select
+                    bg={Colors.blue}
+                    borderWidth={0}
+                    rounded={5}
+                    py={3}
+                    placeholder="Choose Rate"
+                    _selectedItem={{
+                        bg: Colors.babyblue,
+                        endIcon: <CheckIcon size={3} />
+                    }}
+                    >
+                        <Select.Item label="1 - Poor" value="1" />
+                        <Select.Item label="2 - Fair" value="2" />
+                        <Select.Item label="3 - Good" value="3" />
+                    </Select>
+                </VStack>
         </Box>
     </Box>
   
